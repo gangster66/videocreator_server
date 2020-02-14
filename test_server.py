fab2f73f958c947e0441689b7063c0da5233d7f1
gangster66 <61025119+gangster66@users.noreply.github.com>
2020-02-14 20:36:31 +0200

    Add files via upload

--- test_server.py
+++ test_server.py
@@ -1,11 +1,12 @@
 import socket
 import ssl
 
-sock = ssl.wrap_socket(socket.socket(), 'keys/private.key', 'keys/server.crt', True)
-sock.bind( ('localhost', 43433) )
+sock = socket.socket()
+sock.bind(('', 43433))
 sock.listen(10)
 
 while True:
     conn, addr = sock.accept()
+    print(conn)
     data = conn.recv(1024)
     print(data)
